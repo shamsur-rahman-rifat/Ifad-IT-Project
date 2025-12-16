d77313ee2d309514a0452d952f97975495c347ff
shamsur-rahman-rifat <shamsur30rahman@gmail.com>
2025-12-16 22:33:36 +0600

    CDN Plugin Updated

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,16 +1,11 @@
 export default ({ env }) => ({
   upload: {
     config: {
-      provider: "cloudinary",
+      provider: 'cloudinary',
       providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
       },
     },
   },
